36a27d7aad44bf543e66ea6d12c6c0900ec15531
Cornel <plziemniak312pl@gmail.com>
2024-03-06 10:52:11 +0100

    asd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
 import HomePage from './pages/HomePage/HomePage';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
-import Navbar from './components/Navbar/Navbar';
 import AuthPage from './pages/AuthPage/AuthPage';
 import ExplorePage from './pages/ExplorePage/ExplorePage';
 
